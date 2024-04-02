c1e111c069971659e3a9aaa859221dd4ad26b7a9
Naman Chhibbar <n.chhibar@gmail.com>
2024-04-02 07:56:22 +0530

    Added comments in train.py

--- train.py
+++ train.py
@@ -11,20 +11,25 @@ from utils import load_data, load_model, train_val_test_split, train_and_validat
 def main():
     print()
 
+    # Load data
     data = load_data(DATA_PATHS, SHUFFLE)
     train_data, val_data, test_data = train_val_test_split(data, TRAIN_RATIO, VAL_RATIO)
 
+    # Load model and tokenizer
     model, tokenizer = load_model(MODEL, MODEL_DIR)
 
+    # Optimizer and scheduler
     optimizer = torch.optim.Adam(model.parameters(), lr=INIT_LR)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=SCH_STEP, gamma=SCH_GAMMA, verbose=True)
 
+    # Training loop
     train_loss, val_metrics, test_metrics = train_and_validate(
         train_data, val_data, test_data, model, tokenizer,
         MAX_TOKENS, optimizer, scheduler,
         BATCH_SIZE, EPOCHS, FLT_PREC, WHITE_SPACE
     )
 
+    # Plot metrics
     x = range(len(train_loss))
     plt.plot(x, train_loss, label="Train loss")
     plt.plot(x, val_metrics["loss"], label="Validation loss")
@@ -36,9 +41,11 @@ def main():
     plt.savefig(PLOT_PATH)
     print(f"Performance plot saved as {PLOT_PATH}")
 
+    # Save fine-tuned model
     model.save_pretrained(f"{MODEL_DIR}/model")
     print(f"{MODEL} model trained and saved in {MODEL_DIR}\n")
 
+    # Print test metrics
     print(
         "Performance metrics on test set:\n"
         f"Test loss = {round(test_metrics["loss"], FLT_PREC)}\n"
